25a5ac935faf9644176a189dde685587231c08fb
Kelvin Ben <kevoben14@gmail.com>
2022-09-28 22:42:51 +0300

    Add webpack files

--- webpack.config.js
+++ webpack.config.js
@@ -23,10 +23,15 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
     ],
   },
   optimization: {
     runtimeChunk: 'single',
   },
-  mode: 'development'
+  mode: 'development',
 };
