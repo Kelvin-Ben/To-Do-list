a414bcb228204085a358ff8417c5edd4e8a0abbf
Kelvin Ben <kevoben14@gmail.com>
2022-09-30 05:33:47 +0300

    Add modules,html element and style properties

--- src/index.js
+++ src/index.js
@@ -1,6 +1,122 @@
 import './style.css';
-import makeTasks from './modules/makeTasks.js';
+import TaskList from './modules/tasklist.js';
 
-window.onload = () => {
-  makeTasks();
-};
+const taskinput = document.querySelector('.add_todo_input');
+const listContainer = document.querySelector('.todolist');
+const todoaddbutton = document.querySelector('.todo_add_icon');
+const refreshIcon = document.querySelector('.refresh_div');
+const tasklist = new TaskList();
+
+function generateTaskList() {
+  listContainer.innerHTML = tasklist.generateTaskList();
+}
+function refreshTodoList() {
+  listContainer.innerHTML = '';
+  generateTaskList();
+}
+function add() {
+  if (taskinput.value !== '') {
+    tasklist.addTask(taskinput.value);
+    generateTaskList();
+    taskinput.value = '';
+  }
+}
+function hideComponent(elmntsParam) {
+  const test = elmntsParam.classList;
+  if (!test.contains('hidden_item')) {
+    test.toggle('hidden_item');
+  }
+}
+
+function displayComponent(elmnts) {
+  if (elmnts.isArray) {
+    elmnts.forEach((elmnt) => {
+      const test = elmnt.classList;
+      if (test.contains('hidden_item')) {
+        test.remove('hidden_item');
+      }
+    });
+  } else {
+    const test = elmnts.classList;
+    if (test.contains('hidden_item')) {
+      test.remove('hidden_item');
+    }
+  }
+}
+function displayLineThroughComponent(elmntsParam) {
+  const test = elmntsParam.classList;
+  if (!test.contains('line_through')) {
+    test.toggle('line_through');
+  }
+}
+
+function removeLineThroughComponent(elmnts) {
+  if (elmnts.isArray) {
+    elmnts.forEach((elmnt) => {
+      const test = elmnt.classList;
+      if (test.contains('line_through')) {
+        test.remove('line_through');
+      }
+    });
+  } else {
+    const test = elmnts.classList;
+    if (test.contains('line_through')) {
+      test.remove('line_through');
+    }
+  }
+}
+
+function completeTask(index) {
+  hideComponent(document.querySelector(`.uncomplete${index}`));
+  displayComponent(document.querySelector(`.complete${index}`));
+  hideComponent(document.querySelector(`.details${index}`));
+  displayComponent(document.querySelector(`.delete${index}`));
+  displayLineThroughComponent(document.querySelector(`.input${index}`));
+  // console.log(tasklist.localData[index-1].getCompleted());
+  tasklist.localData[index - 1].setCompleted(true);
+}
+
+function uncompleteTask(index) {
+  hideComponent(document.querySelector(`.complete${index}`));
+  displayComponent(document.querySelector(`.uncomplete${index}`));
+  hideComponent(document.querySelector(`.delete${index}`));
+  displayComponent(document.querySelector(`.details${index}`));
+  removeLineThroughComponent(document.querySelector(`.input${index}`));
+  tasklist.localData[index - 1].setCompleted(false);
+}
+
+function focusInput(index) {
+  hideComponent(document.querySelector(`.details${index}`));
+  displayComponent(document.querySelector(`.delete${index}`));
+}
+
+function lifocusout(index) {
+  hideComponent(document.querySelector(`.delete${index}`));
+  displayComponent(document.querySelector(`.details${index}`));
+}
+
+function taskUpdate(index) {
+  tasklist.localData[index - 1].description = document.querySelector(`.input${index}`).value;
+  tasklist.refreshIndex();
+}
+
+function removeTask(index) {
+  tasklist.removeItem(index - 1);
+  refreshTodoList();
+}
+function clearCompletedTasks() {
+  tasklist.clearCompletedTasks();
+  refreshTodoList();
+}
+document.completeTask = completeTask;
+document.uncompleteTask = uncompleteTask;
+document.focusInput = focusInput;
+document.lifocusout = lifocusout;
+document.taskUpdate = taskUpdate;
+document.removeTask = removeTask;
+document.displayLineThroughComponent = displayLineThroughComponent;
+document.removeLineThroughComponent = removeLineThroughComponent;
+document.addEventListener('DOMContentLoaded', refreshTodoList);
+document.querySelector('.todo_clear_all_completed').addEventListener('click', clearCompletedTasks);
+todoaddbutton.addEventListener('click', add);
+refreshIcon.addEventListener('click', refreshTodoList);
--- src/modules/createHtmlelement.js
+++ src/modules/createHtmlelement.js
@@ -1,8 +1,21 @@
-export default (newElement, id, className, innerHTML, parent) => {
+const createHTMLElement = (newElement, className, id, inHTML, parent) => {
   const element = document.createElement(`${newElement}`);
-  element.id = `${id}`;
   element.className = `${className}`;
-  element.innerHTML = `${innerHTML}`;
+  element.id = `${id}`;
+  element.innerHTML = `${inHTML}`;
   parent.appendChild(element);
   return element;
 };
+
+const createInputElement = (newElement, className, id, type, name, value, parent) => {
+  const element = document.createElement(`${newElement}`);
+  element.className = `${className}`;
+  element.id = `${id}`;
+  element.type = `${type}`;
+  element.name = `${name}`;
+  element.value = `${value}`;
+  parent.appendChild(element);
+  return element;
+};
+
+export { createHTMLElement, createInputElement };
