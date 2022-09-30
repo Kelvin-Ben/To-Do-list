f5a261bb04c1e7f490e95c8e9838c73bc24789a1
Kelvin Ben <kevoben14@gmail.com>
2022-09-30 18:15:04 +0300

    Add loocal storage

--- src/modules/tasklist.js
+++ src/modules/tasklist.js
@@ -2,7 +2,7 @@ import Task from './task.js';
 
 export default class TaskList {
   constructor() {
-    this.localData = JSON.parse(localStorage.getItem('tasklist')) || [];
+    localStorage.setItem('tasklist', JSON.stringify(this.localData));
   }
 
   generateTodoIndex() {
